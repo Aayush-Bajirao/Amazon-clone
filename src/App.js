da072f72f09a12bbb20a2bfd4ffbaf33dcfe2824
Aayush Bajirao <aayubaji1208@gmail.com>
2022-03-31 15:21:48 +0530

    React-Dom-Router setup complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div className="App">      
+      <h1>Amazon Clone</h1> 
+      <Routes>
+        <Route path="/checkout" element={<h1>Checkout</h1>} />
+        <Route path="/login" element={<h1>Login page</h1>} />
+        <Route path="/" element={<h1>Home page</h1>} />
+
+      </Routes>
     </div>
+    </Router>
   );
 }
 
